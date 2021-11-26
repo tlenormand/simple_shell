4f166a1611ee11a4136715532717e966883eee4e
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-26 08:32:23 +0000

    first small simple shell

--- get_strtok.c
+++ get_strtok.c
@@ -1,20 +1,90 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * main - splits a string and returns an array of each word of the string
- * Return: Always 0.
+ * strtow - function that splits a string into words
+ * @str: string
+ * Return: array of string
  */
 
-char *get_strtok(char *str)
+char **strtow(char *str)
 {
-	const char *delimiters = " ";
-	char *strToken = strtok ( str, delimiters);
+	char **list = NULL;
+	int i = 0, idx = 0, lenght = 0, row = 0;
 
-	while (strToken != NULL )
+	if (str == NULL || *str == '\0' || *str == '\n')
+		return (NULL);
+
+	row = nb_word(i, str);
+	list = (char **)malloc((sizeof(char *) * nb_word(i, str)));
+	if (list == NULL || row == 0)
 	{
-		strToken = strtok (NULL, delimiters);
+		free(list);
+		return (NULL);
 	}
 
-	return (strToken);
+	row = 0;
+	while (str[i] != '\n')
+	{
+		if (str[i] != ' ')
+		{
+			list[row] = (char *)malloc((2 + nb_letter(i, str) * sizeof(char)));
+			if (list[row] == NULL)
+			{
+				for (idx = 0; idx <= row; idx++)
+					free(list[idx]);
+				free(list);
+				return (NULL);
+			}
+			lenght = 0;
+			for (idx = i; str[idx] != ' ' && str[idx] != '\n' && str[idx]; idx++)
+			{
+				list[row][lenght] = str[idx];
+				lenght++, i++;
+			}
+			row++;
+		}
+		else
+			i++;
+	}
+
+	return (list);
+}
+
+
+/**
+ * nb_letter - count the number of letter in the word
+ * @i: idx of str
+ * @str: string
+ * Return: the number of letter
+ */
+
+int nb_letter(int i, char *str)
+{
+	int letter = 0;
+
+	while (str[i] != ' ' && str[i] != '\0' && str[i] != '\n')
+		letter++, i++;
+
+	return (letter);
+}
+
+
+/**
+ * nb_word - count the number of words in str
+ * @i: idx of str
+ * @str: string
+ * Return: the number of words
+ */
+
+int nb_word(int i, char *str)
+{
+	int row = 0;
+
+	while (str[i] && str[i] != '\n')
+		if (str[i] != ' ')
+			row++, i += nb_letter(i, str);
+		else
+			i++;
+
+	return (row);
 }
--- prompt.c
+++ prompt.c
@@ -2,24 +2,59 @@
 
 /**
  * main - prints "$ ", wait for the user, prints it on the next line
- * Return: number of characteres read
+ * Return: always 0
  */
 
 int main(void)
 {
 	size_t len = 0;
 	char *line = NULL;
-	char *argv;
 
 	printf("$ ");
 	while ((getline(&line, &len, stdin)) != -1)
 	{
-		argv = get_strtok(line);
-		printf("%s", argv);
+		fork_main(line);
 		printf("$ ");
 	}
 
 	printf("\n");
 	free(line);
+	return (0);
+}
+
+
+/**
+ * fork_main - fork the main function
+ * @line: input string got from the prompt
+ * Return: 0 if success, on error -1
+ */
+
+int fork_main(char *line)
+{
+	pid_t child_pid;
+	char **argv;
+	int *end_fils;
+
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		perror("Error: fork()");
+		return (1);
+	}
+
+	if (child_pid == 0)
+	{
+		argv = strtow(line);
+		if (argv != NULL)
+		{
+			exe_func(argv);
+		}
+	}
+	else
+	{
+		wait(end_fils);
+	}
+
 	return (0);
 }
